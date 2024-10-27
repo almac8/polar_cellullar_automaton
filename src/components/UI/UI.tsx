3807a11f45ec2b8a56b593b8659e7a25f4e97f3e
Aimée MacDonald <aimeelmacdonald@pm.me>
2024-10-27 12:48:05 +0200

    UI auto open and close

--- src/components/UI/UI.tsx
+++ src/components/UI/UI.tsx
@@ -1,9 +1,19 @@
+import { useState } from "react";
+
 import "./UI.css";
 
-const UI = () => (
-  <div id="UI">
-    <h1>UI</h1>
-  </div>
-);
+const UI = () => {
+  const [ isOpen, setIsOpen ] = useState(false);
+
+  return (
+    <div
+      id="UI"
+      className={ isOpen ? "open" : "closed" }
+      onMouseEnter={ () => setIsOpen(true) }
+      onMouseLeave={ () => setIsOpen(false) }>
+      {isOpen && <h1>UI</h1>}
+    </div>
+  )
+};
 
 export default UI;
